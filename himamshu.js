1b20e5a4d76bd8541c91382005e17cfcb9a15399
HimanshuJha <jha.himanshu966@gmail.com>
2023-05-28 13:50:05 +0530

    fat arrow added

--- himamshu.js
+++ himamshu.js
@@ -1,5 +1,8 @@
 console.log("ravi ka");
 console.log("Himanshu jhagituuuuu");
 console.log("mai Himanshu Hu mera coede rkhna hai
-")
- 
+") 
+
+const myFunc =()=>{
+    console.log("Fat arrow added")
+}
